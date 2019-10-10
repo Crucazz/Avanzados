b6ec8752448526c1871a4fdffcbff9b0d1a5dbab
Felipe Villalobos <felipe.villalobos.p@usach.cl>
2019-10-09 23:46:02 -0300

    la pera por fingeso es mayor

--- Lab_1/felipeVillaloboa_201393108.c
+++ Lab_1/felipeVillaloboa_201393108.c
@@ -22,9 +22,9 @@ O(n**2)
 void imprimir(int* ciudad[], int ejeX, int ejeY)
 {
 	printf("\n");
-	for(int i=0; i<ejeX; i++)
+	for(int j=0; j<ejeY; j++)
 	{
-		for(int j=0; j<ejeY; j++)
+		for(int i=0; i<ejeX; i++)
 		{
 			printf(" %d ",ciudad[i][j]);
 		}
@@ -40,38 +40,45 @@ void imprimir(int* ciudad[], int ejeX, int ejeY)
 //   reinas - Vector con las posiciones de las reinas
 //   n      - Número de reinas
 
-void backtracking(int fila, int reinas[], int n)
+void backtracking(int* ciudad[], int* mejorCiudad[], int ejeX, int ejeY, int x, int y, int sucActual, int sucMaximas)
 {
-  int ok = 0;
+	int ok = 0;
+	if(sucActual != sucMaximas && listo==0)
+	{
+		for(int j=y; j<ejeY; j++)
+		{
+			for(int i=x; i<ejeX; i++)
+			{
+				if(comprobar(fila,reinas,n) && listo==0)
+				backtracking(int* ciudad[], int* mejorCiudad[], int ejeX, int ejeY, int x, int y, int sucActual, int sucMaximas)
+			}
+		}
+		for (reinas[fila]=0; reinas[fila]<n; reinas[fila]++) 
+	    {
+			// Comprobamos si la posición 
+			// de la reina actual es válida
+	      
+			if (comprobar(fila,reinas,n) && listo==0) 
+			{
 
-  if (fila<n && listo==0) 
-  {
-    // Quedan reinas por colocar
-    for (reinas[fila]=0; reinas[fila]<n; reinas[fila]++) 
-    {
-      // Comprobamos si la posición 
-      // de la reina actual es válida
-      
-      if (comprobar(fila,reinas,n) && listo==0) 
-      {
+			// Si es así, intentamos colocar
+			// las reinas restantes
+			mostrarTablero(reinas,n);
+			colocarReina (fila+1, reinas, n);
 
-        // Si es así, intentamos colocar
-        // las reinas restantes
-        mostrarTablero(reinas,n);
-        colocarReina (fila+1, reinas, n);
-
-      }
-    } 
+			}
+	    } 
   
-  } 
-  else
-  {
-    // No quedan reinas por colocar (solución)
+	} 
+	else
+	{
+    	// Se colocan el maximo de sucursales en la ciudad
+		//no hay mas opciones, por lo que se retornara desde este punto
     
-    mostrarTablero(reinas,n);
-    listo=1;
-    printf("\nEsto es todo amigos\n");
-    return;
+		mostrarTablero(reinas,n);
+		listo=1;
+		printf("\nEsto es todo amigos\n");
+		return;
   }
 
   //return ok;
@@ -85,6 +92,7 @@ void main (int argc, char *argv[])
 
 	int ejeX =atoi(argv[1]);
 	int ejeY =atoi(argv[2]);
+	
 
 	//se queda con el minimo tamaño entre ancho y alto
 	if( ejeX>ejeY ) 
@@ -95,9 +103,11 @@ void main (int argc, char *argv[])
 
     // Se crea la la ciudad segun el tamaño ingresado
 	int **ciudad = (int **)malloc(sizeof(int*)*ejeX);
+	int **mejorCiudad = (int **)malloc(sizeof(int*)*ejeX);
 	for(int i=0; i<ejeX; i++)
 	{
 		ciudad[i]=(int *)malloc(sizeof(int)*ejeY);
+		mejorCiudad[i]=(int *)malloc(sizeof(int)*ejeY);
 	}
 
 
@@ -113,14 +123,19 @@ void main (int argc, char *argv[])
 		}
 	}
 
-	imprimir(ciudad,ejeX ,ejeY );
-    backtracking(0,ciudad,sucursales);
+	ciudad[0][1]=2;
+	imprimir(ciudad,ejeX ,ejeY, mejorCiudad);
 
+    //backtracking(ciudad,ejeX ,ejeY, mejorCiudad);
+
+	//Liberacion de memoria
     for(int i=0; i<ejeX; i++) 
 	{
 		free(ciudad[i]);
+		free(mejorCiudad[i]);
 	}
 	free(ciudad);
+	free(mejorCiudad);
 
 
 	
