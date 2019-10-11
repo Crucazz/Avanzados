f75a7b7b50bd2959c406049fe7ba8d0cd1c76885
Felipe Villalobos <felipe.villalobos.p@usach.cl>
2019-10-10 23:55:28 -0300

    Falta revisar la diagonal del numero y listo, no se me ocurre como

--- Lab_1/felipeVillaloboa_201393108.c
+++ Lab_1/felipeVillaloboa_201393108.c
@@ -32,14 +32,51 @@ void imprimir(int* ciudad[], int ejeX, int ejeY)
 	}
 }
 
+int comprobar(int* ciudad[],int ejeX,int ejeY,int x,int y)
+{
+	//comprueba vertical
+	for(int j=0; j<ejeY; j++)
+	{
+		if(ciudad[x][j]!=0)	
+		{
+			printf("No se puede colocar en %d,%d\n",x,j );
+			return (0);
+		}
+		
+	}	
 
-// Colocación de una reina
-// -----------------------
-// Parámetros
-//   fila   - Fila de la reina que queremos colocar
-//   reinas - Vector con las posiciones de las reinas
-//   n      - Número de reinas
+	//comprueba horizontal
+	for(int i=0; i<ejeX; i++)
+	{
+		if(ciudad[i][y]!=0)	
+		{
+			printf("No se puede colocar en %d,%d\n",i,y );
+			return (0);
+		}		
+		if(ciudad[i][i]!=0)	
+		{
+			printf("No se puede colocar (diagonal pricnipal) en %d,%d\n",i,i );
+			return (0);
+		}
+	}
+	return (1);
 
+}
+
+/*
+Descripcion: Colocación de una sucursal en la ciudad
+ ---------------------------
+Parámetros:
+	ciudad   - Fila de la reina que queremos colocar
+	mejorCiudad - Vector con las posiciones de las reinas
+	ejeX      - Número de reinas
+	ejeY      - Número de reinas
+	x      - Número de reinas
+	y      - Número de reinas
+	sucActaul      - Número de sucursales en actuales en la ciudad
+	sucMaximas      - Número maximo de sucursales posibles en la ciudad
+
+*/
 void backtracking(int* ciudad[], int* mejorCiudad[], int ejeX, int ejeY, int x, int y, int sucActual, int sucMaximas)
 {
 	int ok = 0;
@@ -49,33 +86,28 @@ void backtracking(int* ciudad[], int* mejorCiudad[], int ejeX, int ejeY, int x, 
 		{
 			for(int i=x; i<ejeX; i++)
 			{
-				if(comprobar(fila,reinas,n) && listo==0)
-				backtracking(int* ciudad[], int* mejorCiudad[], int ejeX, int ejeY, int x, int y, int sucActual, int sucMaximas)
+				printf("\nPosc X: %d  POscY: %d\n",i,j);
+				//¿es posible colocar la sucursal?
+				if(listo==0 && comprobar(ciudad,ejeX,ejeY,i,j)==1)
+				{
+					printf("\nSe puede colocar\n");
+					sucActual=sucActual+1;
+					ciudad[i][j]=1;
+					imprimir(ciudad,ejeX ,ejeY);
+					backtracking(ciudad,mejorCiudad,ejeX,ejeY,0,j+1,sucActual,sucMaximas);
+
+				}
+				
 			}
 		}
-		for (reinas[fila]=0; reinas[fila]<n; reinas[fila]++) 
-	    {
-			// Comprobamos si la posición 
-			// de la reina actual es válida
-	      
-			if (comprobar(fila,reinas,n) && listo==0) 
-			{
 
-			// Si es así, intentamos colocar
-			// las reinas restantes
-			mostrarTablero(reinas,n);
-			colocarReina (fila+1, reinas, n);
-
-			}
-	    } 
-  
 	} 
 	else
 	{
     	// Se colocan el maximo de sucursales en la ciudad
 		//no hay mas opciones, por lo que se retornara desde este punto
     
-		mostrarTablero(reinas,n);
+		imprimir(ciudad,ejeX ,ejeY);
 		listo=1;
 		printf("\nEsto es todo amigos\n");
 		return;
@@ -119,14 +151,15 @@ void main (int argc, char *argv[])
 	{
 		for(int j=0; j<ejeY; j++)
 		{
+			mejorCiudad[i][j]=0;
 			ciudad[i][j]=0;
 		}
 	}
 
-	ciudad[0][1]=2;
-	imprimir(ciudad,ejeX ,ejeY, mejorCiudad);
+//	ciudad[0][1]=1;
+	imprimir(ciudad,ejeX ,ejeY);
 
-    //backtracking(ciudad,ejeX ,ejeY, mejorCiudad);
+    backtracking(ciudad,mejorCiudad,ejeX,ejeY,0,0,0,sucursales);
 
 	//Liberacion de memoria
     for(int i=0; i<ejeX; i++) 
